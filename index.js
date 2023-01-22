82142a687aab537bc66bdc5b946724fb83911fd5
Ysh-rael_linux <ysp.rael@gmail.com>
2023-01-22 01:27:51 -0300

    add routers get

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 import Express from 'express'
-import {$transformByObj, $transformByURL} from './routers/main.js'
+import { $transformByObj, $transformByURL } from './routers/main.js'
 
-const port = process.env.port || 3006
+const port = process.env.port || 3008
 const app = Express()
 app.use(Express.json())
 
 app.post('/transformbyobj', $transformByObj)
 app.post('/transformbyurl', $transformByURL)
-app.get('/', (req,res) => res.send({}))
 
-app.listen(port , () => console.info(`🚀 Running in port ${port}`,))
+app.get('/transformbyobj', (req, res) => res.send({ Warning: "POST method must be used on this route." }))
+app.get('/transformbyurl', (req, res) => res.send({ Warning: "POST method must be used on this route." }))
+app.get('/', (req, res) => res.send({ Warning: "Access the /transformbyobj or /transformbyurl routes using the POST method to use this API." }))
+
+app.listen(port, () => console.info(`🚀 Running in port ${port}`,))
